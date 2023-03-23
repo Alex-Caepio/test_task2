2ab155ff354f50204568a32a397e0a040992028a
Aleksei <mjbcaepio@gmail.com>
2023-03-23 12:24:16 +0300

    An initial commit of a test task

---
+++ SignatureNote.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'Signature.php';
+
+enum Color: string
+{
+    public const RED = 'red';
+    public const GREEN = 'green';
+}
+
+class SignatureNote implements Signature
+{
+    public string $name;
+    public string $phoneOne;
+    public string $phoneTwo;
+    public string $emailOne;
+    public string $emailTwo;
+    public string $color;
+
+    public function __construct(
+        string $name,
+        string $phoneOne,
+        string $phoneTwo,
+        string $emailOne,
+        string $emailTwo,
+        string $color
+    ) {
+        $this->name = $name;
+        $this->phoneOne = $phoneOne;
+        $this->phoneTwo = $phoneTwo;
+        $this->emailOne = $emailOne;
+        $this->emailTwo = $emailTwo;
+        $this->color = $color;
+    }
+
+    public function getSignature(): string
+    {
+        echo '<div style="text-align: left;">-----------</div>';
+        return '<div style="color: ' . $this->color . '; line-height: 0.3; text-decoration:none;">
+        <p>С Уважением,</p>
+        <p>' . $this->name . '</p>
+        <p>Тел:</p>
+        <p><a style="color: ' . $this->color . '; text-decoration: none;" href="tel: +375291234567"> ' . $this->formatPhoneOne() . ' </a></p>
+        <p><a style="color: ' . $this->color . '; text-decoration: none;" href="tel: +375297654321"> ' . $this->formatPhoneTwo() . '</a></p>
+        <br>
+        <p>E-Mail:</p>
+        <p><a style="color: ' . $this->color . '; text-decoration: none" href="mailto: mjbcaepio@gmail.com"> ' . $this->emailOne . ' </a></p>
+        <p><a style="color: ' . $this->color . '; text-decoration: none" href="mailto: caepioman@gmail.com"> ' . $this->emailTwo . '</a></p>
+        <br>
+        <br>
+        </div>';
+    }
+
+    public function formatPhoneNumber($number): array|string|null
+    {
+        $number = preg_replace("/\D/", "", $number);
+        if (strlen($number) == 11) {
+            return preg_replace("/([0-9]{3})([0-9]{2})([0-9]{3})([0-9]{2})([0-9]{2})/", "+$1 ($2) $3-$4-$5", $number);
+        } else {
+            return preg_replace("/([0-9]{3})([0-9]{2})([0-9]{3})([0-9]{2})([0-9]{2})/", "+$1 ($2) $3-$4-$5", $number);
+        }
+    }
+
+    public function formatPhoneOne(): string
+    {
+        return $this->formatPhoneNumber($this->phoneOne);
+    }
+
+    public function formatPhoneTwo(): string
+    {
+        return $this->formatPhoneNumber($this->phoneTwo);
+    }
+}
+
+$signature1 = new SignatureNote(
+    'Брутов А.А.',
+    '375295038183',
+    '375295038184',
+    'mjbcaepio@gmail.com',
+    'caepioman@gmail.com',
+    Color::RED
+);
+$signature2 = new SignatureNote(
+    'Брутов А.А.',
+    '375295038183',
+    '375295038184',
+    'mjbcaepio@gmail.com',
+    'caepioman@gmail.com',
+    Color::GREEN
+);
+print_r($signature1->getSignature());
+print_r($signature2->getSignature());
